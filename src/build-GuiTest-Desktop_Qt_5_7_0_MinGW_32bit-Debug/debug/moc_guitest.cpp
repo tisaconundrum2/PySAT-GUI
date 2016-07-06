5cb243fdd280a666637401905cb1c729d9275bd3
Nicholas Finch <ngf4@nau.edu>
2016-07-06 14:54:51 -0700

    Update: 2016-07-06_0254 PM

--- src/build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_guitest.cpp
+++ src/build-GuiTest-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_guitest.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_GuiTest_t {
-    QByteArrayData data[17];
-    char stringdata0[305];
+    QByteArrayData data[18];
+    char stringdata0[265];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,32 +30,32 @@ struct qt_meta_stringdata_GuiTest_t {
 static const qt_meta_stringdata_GuiTest_t qt_meta_stringdata_GuiTest = {
     {
 QT_MOC_LITERAL(0, 0, 7), // "GuiTest"
-QT_MOC_LITERAL(1, 8, 21), // "on_toolButton_clicked"
-QT_MOC_LITERAL(2, 30, 0), // ""
-QT_MOC_LITERAL(3, 31, 23), // "on_toolButton_2_clicked"
-QT_MOC_LITERAL(4, 55, 23), // "on_toolButton_3_clicked"
-QT_MOC_LITERAL(5, 79, 23), // "on_toolButton_4_clicked"
-QT_MOC_LITERAL(6, 103, 23), // "on_actionExit_triggered"
-QT_MOC_LITERAL(7, 127, 24), // "on_pushButton_13_clicked"
-QT_MOC_LITERAL(8, 152, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(9, 174, 16), // "setLabelsVisible"
-QT_MOC_LITERAL(10, 191, 1), // "i"
-QT_MOC_LITERAL(11, 193, 7), // "visible"
-QT_MOC_LITERAL(12, 201, 19), // "setSpinrightVisible"
-QT_MOC_LITERAL(13, 221, 18), // "setSpinleftVisible"
-QT_MOC_LITERAL(14, 240, 30), // "on_norm_spinBox_9_valueChanged"
-QT_MOC_LITERAL(15, 271, 4), // "arg1"
-QT_MOC_LITERAL(16, 276, 28) // "on_norm_spinBox_valueChanged"
+QT_MOC_LITERAL(1, 8, 16), // "setLabelsVisible"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 5), // "index"
+QT_MOC_LITERAL(4, 32, 7), // "visible"
+QT_MOC_LITERAL(5, 40, 19), // "setSpinrightVisible"
+QT_MOC_LITERAL(6, 60, 18), // "setSpinleftVisible"
+QT_MOC_LITERAL(7, 79, 21), // "on_toolButton_clicked"
+QT_MOC_LITERAL(8, 101, 23), // "on_toolButton_2_clicked"
+QT_MOC_LITERAL(9, 125, 23), // "on_toolButton_3_clicked"
+QT_MOC_LITERAL(10, 149, 23), // "on_toolButton_4_clicked"
+QT_MOC_LITERAL(11, 173, 23), // "on_actionExit_triggered"
+QT_MOC_LITERAL(12, 197, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(13, 222, 14), // "SpinBoxChanged"
+QT_MOC_LITERAL(14, 237, 8), // "QWidget*"
+QT_MOC_LITERAL(15, 246, 3), // "wSp"
+QT_MOC_LITERAL(16, 250, 12), // "spinboxWrite"
+QT_MOC_LITERAL(17, 263, 1) // "i"
 
     },
-    "GuiTest\0on_toolButton_clicked\0\0"
-    "on_toolButton_2_clicked\0on_toolButton_3_clicked\0"
-    "on_toolButton_4_clicked\0on_actionExit_triggered\0"
-    "on_pushButton_13_clicked\0on_pushButton_clicked\0"
-    "setLabelsVisible\0i\0visible\0"
+    "GuiTest\0setLabelsVisible\0\0index\0visible\0"
     "setSpinrightVisible\0setSpinleftVisible\0"
-    "on_norm_spinBox_9_valueChanged\0arg1\0"
-    "on_norm_spinBox_valueChanged"
+    "on_toolButton_clicked\0on_toolButton_2_clicked\0"
+    "on_toolButton_3_clicked\0on_toolButton_4_clicked\0"
+    "on_actionExit_triggered\0"
+    "on_pushButton_13_clicked\0SpinBoxChanged\0"
+    "QWidget*\0wSp\0spinboxWrite\0i"
 };
 #undef QT_MOC_LITERAL
 
@@ -65,7 +65,7 @@ static const uint qt_meta_data_GuiTest[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      12,   14, // methods
+      11,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -73,32 +73,30 @@ static const uint qt_meta_data_GuiTest[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   74,    2, 0x08 /* Private */,
-       3,    0,   75,    2, 0x08 /* Private */,
-       4,    0,   76,    2, 0x08 /* Private */,
-       5,    0,   77,    2, 0x08 /* Private */,
-       6,    0,   78,    2, 0x08 /* Private */,
-       7,    0,   79,    2, 0x08 /* Private */,
-       8,    0,   80,    2, 0x08 /* Private */,
-       9,    2,   81,    2, 0x08 /* Private */,
-      12,    2,   86,    2, 0x08 /* Private */,
-      13,    2,   91,    2, 0x08 /* Private */,
-      14,    1,   96,    2, 0x08 /* Private */,
-      16,    1,   99,    2, 0x08 /* Private */,
+       1,    2,   69,    2, 0x08 /* Private */,
+       5,    2,   74,    2, 0x08 /* Private */,
+       6,    2,   79,    2, 0x08 /* Private */,
+       7,    0,   84,    2, 0x08 /* Private */,
+       8,    0,   85,    2, 0x08 /* Private */,
+       9,    0,   86,    2, 0x08 /* Private */,
+      10,    0,   87,    2, 0x08 /* Private */,
+      11,    0,   88,    2, 0x08 /* Private */,
+      12,    0,   89,    2, 0x08 /* Private */,
+      13,    1,   90,    2, 0x08 /* Private */,
+      16,    1,   93,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool,    3,    4,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool,    3,    4,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool,    3,    4,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::Int, QMetaType::Bool,   10,   11,
-    QMetaType::Void, QMetaType::Int, QMetaType::Bool,   10,   11,
-    QMetaType::Void, QMetaType::Int, QMetaType::Bool,   10,   11,
-    QMetaType::Void, QMetaType::Int,   15,
-    QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void, 0x80000000 | 14,   15,
+    QMetaType::Void, 0x80000000 | 14,   17,
 
        0        // eod
 };
@@ -109,19 +107,36 @@ void GuiTest::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         GuiTest *_t = static_cast<GuiTest *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_toolButton_clicked(); break;
-        case 1: _t->on_toolButton_2_clicked(); break;
-        case 2: _t->on_toolButton_3_clicked(); break;
-        case 3: _t->on_toolButton_4_clicked(); break;
-        case 4: _t->on_actionExit_triggered(); break;
-        case 5: _t->on_pushButton_13_clicked(); break;
-        case 6: _t->on_pushButton_clicked(); break;
-        case 7: _t->setLabelsVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
-        case 8: _t->setSpinrightVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
-        case 9: _t->setSpinleftVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
-        case 10: _t->on_norm_spinBox_9_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 11: _t->on_norm_spinBox_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 0: _t->setLabelsVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 1: _t->setSpinrightVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 2: _t->setSpinleftVisible((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 3: _t->on_toolButton_clicked(); break;
+        case 4: _t->on_toolButton_2_clicked(); break;
+        case 5: _t->on_toolButton_3_clicked(); break;
+        case 6: _t->on_toolButton_4_clicked(); break;
+        case 7: _t->on_actionExit_triggered(); break;
+        case 8: _t->on_pushButton_13_clicked(); break;
+        case 9: _t->SpinBoxChanged((*reinterpret_cast< QWidget*(*)>(_a[1]))); break;
+        case 10: _t->spinboxWrite((*reinterpret_cast< QWidget*(*)>(_a[1]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 9:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QWidget* >(); break;
+            }
+            break;
+        case 10:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QWidget* >(); break;
+            }
+            break;
         }
     }
 }
@@ -151,13 +166,13 @@ int GuiTest::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 12)
+        if (_id < 11)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 12;
+        _id -= 11;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 12)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 12;
+        if (_id < 11)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 11;
     }
     return _id;
 }
