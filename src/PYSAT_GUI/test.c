8c4ab53c226267b6d02224e404b35e4a4b93de6e
Nicholas Finch <ngf4@nau.edu>
2016-07-11 16:51:00 -0700

    Update: 2016-07-11_0450 PM

--- src/PYSAT_GUI/test.c
+++ src/PYSAT_GUI/test.c
@@ -19,3 +19,29 @@ enum StringValue {norm_label,
                   norm_spinBox_15,
                   norm_spinBox_16
                  };
+static std::map<std::string, StringValue> s_mapStringValues;
+static char szInput[_MAX_PATH];
+static void Initialize();
+
+int main(){
+    Initialize();
+    switch (s_mapStringValues[szInput]){
+    case norm_label:
+    case norm_label_2:
+    case norm_label_3:
+    case norm_label_4:
+    case norm_label_5:
+    case norm_label_6:
+    case norm_label_7:
+    case norm_label_8:
+    case norm_label_9:
+    case norm_spinBox_10:
+    case norm_spinBox_11:
+    case norm_spinBox_12:
+    case norm_spinBox_13:
+    case norm_spinBox_14:
+    case norm_spinBox_15:
+    case norm_spinBox_16:
+    }
+
+}
