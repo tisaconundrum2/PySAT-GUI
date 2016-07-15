00239e2805cc5577165d29a3f820f832d437e2a9
Nicholas Finch <ngf4@nau.edu>
2016-07-15 16:59:56 -0700

    Update: 2016-07-15_0459 PM

--- src/PYSAT_GUI/guitestII.cpp
+++ src/PYSAT_GUI/guitestII.cpp
@@ -13,11 +13,84 @@
 #include <QTextStream>
 
 
-int isNormalizationFilled = 0;
-int isSpinBoxFilled = 0;
-int normalizationBtnCnt = 0;
-int normalizationSize = 0;
-int spinArray[16] = {0,0,0,0,
-                     0,0,0,0,
-                     0,0,0,0};
+int isNormalizationFilled = 0,
+isSpinBoxFilled = 0,
+normalizationBtnCnt = 0,
+spinArray[16] = {0,0,0,0,
+                 0,0,0,0,
+                 0,0,0,0};
 
+QString pythonProgrmaFile = "";
+QString outputDirectory = "";
+QFile file("pls_sm_output.py");
+QTextStream output(&file);
+
+GuiTest::GuiTest (QWidget *parent){
+    QMainWindow(parent), ui(new Ui::GuiTest){
+        ui->setupUi(this);
+        setSizeOfWindow();
+        setQSpinWidgets();
+        setOutputFile();
+    }
+}
+
+GuiTest::~GuiTest()(){
+    delete ui;
+}
+
+void setSizeOfWindow(){
+    QRect currentWindowSize = QApplication::desktop()->screenGeometry();
+    int height = currentWindowSize.height();
+    this->resize(this->width(), height*0.9);
+    for (int i = 0; i < norm_size; i++){        //setting up visibility
+        setSpinleftVisible(i, false);
+        setSpinrightVisible(i, false);
+        setLabelsVisible(i, false);
+    }
+}
+
+void GuiTest::setQSpinWidgets(){
+    QList<QSpinBox*> allChildSpinBoxes = findChildren<QSpinBox*>();
+    QSignalMapper* signalMapper = new QSignalMapper(this);
+    QSpinBox oneChildSpinbox;
+    foreach(oneChildSpinbox, allChildSpinBoxes){
+        connect(oneChildSpinbox, SIGNAL(valueChanged(int)), signalMapper, SLOT(map()));
+        signalMapper->setMapping(oneChildSpinbox, oneChildSpinbox);
+    }
+    connect(signalMapper, SIGNAL(mapped(QWidget*)), this, SLOT(getSpinboxValue(QWidget*)));
+}
+
+void GuiTest::SetupOutFile(){
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream output(&file);
+    output << "from pysat.spectral.spectral_data import spectral_data\n";
+    output << "from pysat.regression.pls_sm import pls_sm\n";
+    output << "import pandas as pd\n";
+}
+
+void GuiTest::setLabelSpinVisible(int index, bool visible){
+    int arraySize = 7;
+    QLabel* labels[arraySize] = {ui->norm_label_2,
+                                 ui->norm_label_3,
+                                 ui->norm_label_4,
+                                 ui->norm_label_5,
+                                 ui->norm_label_6,
+                                 ui->norm_label_7,
+                                 ui->norm_label_8};
+    QSpinBox* spinBoxR[arraySize] = {ui->norm_spinBox_2,
+                                     ui->norm_spinBox_3,
+                                     ui->norm_spinBox_4,
+                                     ui->norm_spinBox_5,
+                                     ui->norm_spinBox_6,
+                                     ui->norm_spinBox_7,
+                                     ui->norm_spinBox_8};
+    QSpinBox* spinBoxL[arraySize] = {ui->norm_spinBox_10,
+                                     ui->norm_spinBox_11,
+                                     ui->norm_spinBox_12,
+                                     ui->norm_spinBox_13,
+                                     ui->norm_spinBox_14,
+                                     ui->norm_spinBox_15,
+                                     ui->norm_spinBox_16
+                                    };
+    labels[index]->setVisible(visible);
+}
