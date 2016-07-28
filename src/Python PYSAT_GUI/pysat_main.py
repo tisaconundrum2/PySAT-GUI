52ed510a72c7eebc46172df5104b65346d3e8170
Nicholas Finch <ngf4@nau.edu>
2016-07-28 16:43:38 -0700

    Update: 2016-07-28_0443 PM

--- src/Python PYSAT_GUI/pysat_main.py
+++ src/Python PYSAT_GUI/pysat_main.py
@@ -5,7 +5,7 @@ from mainwindow_compranges import CompRanges
 from mainwindow_createmodels import CreateModels
 from mainwindow_files import Files
 from mainwindow_normalization import Normalization
-from mainwindow_setup import  SetUp
+from mainwindow_setup import SetUp
 
 class initUI(QMainWindow):
     def __init__(self):
