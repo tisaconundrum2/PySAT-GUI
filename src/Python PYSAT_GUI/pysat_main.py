b84c8b915aa4cfc4b911dadc0f824743768a655b
Nicholas Finch <ngf4@nau.edu>
2016-07-28 15:31:53 -0700

    Update: 2016-07-28_0331 PM

--- src/Python PYSAT_GUI/pysat_main.py
+++ src/Python PYSAT_GUI/pysat_main.py
@@ -3,14 +3,16 @@ from PyQt4.QtCore import *
 import sys, time
 from mainwindow_compranges import CompRanges
 from mainwindow_createmodels import CreateModels
+from mainwindow_files import Files
+from mainwindow_normalization import Normalization
+from mainwindow_setup import  SetUp
 
-class main(QMainWindow, CompRanges):
+class main(QMainWindow, CreateModels):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
 
 app = QApplication(sys.argv)
 window = main()
-CompRanges.setupUi(QMainWindow, Comp)
 window.show()
 app.exec_()
