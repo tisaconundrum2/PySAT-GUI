5ad1075c9e4ab1eb9c1b626cbb52cbc3052756ab
Nicholas Finch <ngf4@nau.edu>
2016-09-09 12:04:21 -0700

    Update: 2016-09-09_1204 PM

--- src/Python PYSAT_GUI/pysat_ui.py
+++ src/Python PYSAT_GUI/pysat_ui.py
@@ -887,19 +887,13 @@ class pysat_ui(object):
         self.verticalLayout.addLayout(self.verticalLayout_2)
 
         # for debugging purposes
-        self.norm_spinBox_[(count * 2) - 1].editingFinished.connect(lambda: print(self.spinCount((count * 2) - 1)))
-        self.norm_spinBox_[(count * 2)].editingFinished.connect(lambda: print(self.spinCount(count * 2)))
+        self.norm_spinBox_[(count * 2) - 1].editingFinished.connect(lambda: )
+        self.norm_spinBox_[(count * 2)].editingFinished.connect(lambda: )
 
         self.norm_label.setText(_translate("MainWindow", "Value {}".format(self.count), None))
 
 
     ####These functions below are private and add functionality to the UI
-
-    def spinCount(self, value):
-        for i in range(self.count * 2):
-            if value == "norm_spinBox_{}".format(i):
-                return i
-
 
     def on_maskFile_clicked(self):
         filename = QFileDialog.getOpenFileName(None, "Open Mask File", '.', "(*.csv)")
