05f10efde2d980970a8bdd5ae12da32fd66b0850
Nicholas Finch <ngf4@nau.edu>
2016-09-20 17:13:08 -0700

    Auto Generated Update:_

--- src/Python PYSAT_GUI/scratch.py
+++ src/Python PYSAT_GUI/scratch.py
@@ -14,12 +14,6 @@ pysat.set_file_db(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\
 pysat.set_file_unknowndatacsv(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\lab_data_averages_pandas_format.csv')
 pysat.set_file_maskfile(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\mask_minors_noise.csv')
 
-outpath = r'C:\Users\nfinch\Desktop\Output'
-db = r"C:\Users\nfinch\Desktop\full_db_mars_corrected_dopedTiO2_pandas_format.csv"
-unknowndatacsv = r"C:\Users\nfinch\Desktop\lab_data_averages_pandas_format.csv"
-maskfile = r"C:\Users\nfinch\Desktop\mask_minors_noise.csv"
-
-
 
 ###################################################
 # Spectral setup
