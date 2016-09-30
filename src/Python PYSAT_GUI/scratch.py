e064838a092837d742d61626bc6447ecfb547835
Nicholas Finch <ngf4@nau.edu>
2016-09-30 10:30:23 -0700

    Begin the wheels of evolution
    From here on out, I am going to convert pysat_function into a more
    rigorous, flexible and well documented library. I am going to create
    another scratch file to make sure that I can still definitely output
    plots and data correctly.

--- src/Python PYSAT_GUI/scratch.py
+++ src/Python PYSAT_GUI/scratch.py
@@ -9,19 +9,29 @@ pysat.set_file_knowndatacsv(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python 
 pysat.set_file_unknowndatacsv(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\lab_data_averages_pandas_format.csv')
 pysat.set_file_maskfile(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\mask_minors_noise.csv')
 
-
 pysat.get_spectra()
+
 pysat.set_interp()
+
 pysat.set_mask()
+
 pysat.get_ranges([(0, 1000)])
+
 pysat.set_element_name('SiO2')
+
 pysat.set_nfolds(6)
-pysat.set_testfold_test(4)
+
+pysat.set_testfold(4)
+
 pysat.set_compranges([[-20, 50], [30, 70], [60, 100], [0, 120]])
+
 pysat.set_stratified()
+
 pysat.get_number_components([7, 7, 5, 9])
+
 pysat.get_train_data()
 pysat.get_test_data()
+
 pysat.set_sm()
 pysat.get_sm_fit()
 pysat.get_plots()
