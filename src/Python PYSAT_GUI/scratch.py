e0cfaebcda04f091a4c9eee2621dc14b5ceb39a9
Nicholas Finch <ngf4@nau.edu>
2016-10-07 11:49:41 -0700

    Added testing directories
    We can now move forward with seeing if everything works as planned

--- src/Python PYSAT_GUI/scratch.py
+++ src/Python PYSAT_GUI/scratch.py
@@ -7,9 +7,9 @@ pysat = pysat_func()
 
 #get all the files
 f = [pysat.set_file_outpath(r'C:\Users\nfinch\Desktop\Output'),
-pysat.set_file_knowndatacsv(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\full_db_mars_corrected_dopedTiO2_pandas_format.csv'),
-pysat.set_file_unknowndatacsv(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\lab_data_averages_pandas_format.csv'),
-pysat.set_file_maskfile(r'C:\Users\nfinch\Documents\GitHub\PySAT\src\Python PYSAT_GUI\mask_minors_noise.csv'),
+pysat.set_file_knowndatacsv(r'C:\Users\nfinch\Desktop\full_db_mars_corrected_dopedTiO2_pandas_format.csv'),
+pysat.set_file_unknowndatacsv(r'C:\Users\nfinch\Desktop\lab_data_averages_pandas_format.csv'),
+pysat.set_file_maskfile(r'C:\Users\nfinch\Desktop\mask_minors_noise.csv'),
 pysat.get_spectra(),
 pysat.set_interp(),
 pysat.set_mask(),
