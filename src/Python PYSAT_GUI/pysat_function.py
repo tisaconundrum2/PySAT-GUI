bca7b82d0aa1c8c79bf2cdba26cda141ff6761d7
Nicholas Finch <ngf4@nau.edu>
2016-10-14 10:47:31 -0700

    Merging with Ryan's GUI files 	modified:   src/Python PYSAT_GUI/pysat_function.py

--- src/Python PYSAT_GUI/pysat_function.py
+++ src/Python PYSAT_GUI/pysat_function.py
@@ -2,97 +2,161 @@ from pysat.spectral.spectral_data import spectral_data
 from pysat.regression.pls_sm import pls_sm
 import pandas as pd
 
+
 class pysat_func():
-# Thus make sure that you have if's for all instances in functions where unknown_data doesn't exist.
+    # Thus make sure that you have if's for all instances in functions where unknown_data doesn't exist.
+    def __init__(self):
+        self.data={} #initialize with an empty dict to hold data frames
+        self.datakeys=[]
+
     def set_file_outpath(self, outpath):
-        self.outpath = outpath
-        print("Output path folder has been set")
+        try:
+            self.outpath = outpath
+            print("Output path folder has been set")
+        except Exception as e:
+            print(e)
 
     def set_file_knowndatacsv(self, db):
-        self.db = db
-        print("Known data file has been added")
+        try:
+            self.db = db
+            print("Known data file has been added")
+        except Exception as e:
+            print(e)
 
     def set_file_unknowndatacsv(self, unknowndatacsv):
-        self.unknowndatacsv = unknowndatacsv
-        print("Unknown data file has been added")
+        try:
+            self.unknowndatacsv = unknowndatacsv
+            print("Unknown data file has been added")
+        except Exception as e:
+            print(e)
 
     def set_file_maskfile(self, maskfile):
-        self.maskfile = maskfile
-        print("Masking file has been added")
+        try:
+            self.maskfile = maskfile
+            print("Masking file has been added")
+        except Exception as e:
+            print(e)
 
+    def get_data(self,filename,keyname):
+        try:
+            print('Loading data file: '+str(filename))
+            self.data[keyname]=spectral_data(pd.read_csv(filename,header=[0,1]))
+            self.datakeys.append(keyname)
+
+        except Exception as e:
+            print('Problem reading data: {}'.format(e))
+            
+            
     def get_spectra(self):
-        print("Running Spectral on data set")
-        self.data = pd.read_csv(self.db, header=[0, 1])
-        self.data = spectral_data(self.data)
-        self.unknown_data = pd.read_csv(self.unknowndatacsv, header=[0, 1])
-        self.unknown_data = spectral_data(self.unknown_data)
-        print("Spectral analysis has completed")
+        try:
+            print("Running Spectral on data set")
+            self.data = pd.read_csv(self.db, header=[0, 1])
+            self.data = spectral_data(self.data)
+            self.unknown_data = pd.read_csv(self.unknowndatacsv, header=[0, 1])
+            self.unknown_data = spectral_data(self.unknown_data)
+            print("Spectral analysis has completed")
+        except Exception as e:
+            print("I am missing some files please fix: {}".format(e))
 
     def set_interp(self):
         # TODO interp should be it's ownn function
-        self.unknown_data.interp(self.data.df['wvl'].columns)
-        print("Interpolation has been applied")
+        try:
+            self.unknown_data.interp(self.data.df['wvl'].columns)
+            print("Interpolation has been applied")
+        except Exception as e:
+            print(e)
 
     def set_mask(self):
-        self.data.mask(self.maskfile)
-        self.unknown_data.mask(self.maskfile)
-        print("Masking has been applied")
+        try:
+            self.data.mask(self.maskfile)
+            self.unknown_data.mask(self.maskfile)
+            print("Masking has been applied")
+        except Exception as e:
+            print(e)
 
     def get_ranges(self, ranges):
         print("{}".format(ranges))
-        self.data.norm(ranges)
-        self.unknown_data.norm(ranges)
-        print("{}".format(ranges))
-        print("Ranges have been applied")
+        try:
+            self.data.norm(ranges)
+            self.unknown_data.norm(ranges)
+            print("{}".format(ranges))
+            print("Ranges have been applied")
+        except Exception as e:
+            print(e)
+
 
     def set_element_name(self, el):
-        self.el = el
-        print("{}".format(el))
-        print("Element name for y variable has been set")
+        try:
+            self.el = el
+            print("{}".format(el))
+            print("Element name for y variable has been set")
+        except Exception as e:
+            print(e)
 
     def set_nfolds(self, nfolds_test):
-        self.nfolds_test = nfolds_test
-        print("{}".format(nfolds_test))
-        print("N folds has been applied")
+        try:
+            self.nfolds_test = nfolds_test
+            print("{}".format(nfolds_test))
+            print("N folds has been applied")
+        except Exception as e:
+            print(e)
 
     def set_testfold(self, testfold_test):
-        self.testfold_test = testfold_test
-        print("{}".format(testfold_test))
-        print("Test folds has been applied")
+        try:
+            self.testfold_test = testfold_test
+            print("{}".format(testfold_test))
+            print("Test folds has been applied")
+        except Exception as e:
+            print(e)
 
-    def get_nfolds(self):
-        return self.nfolds_test
+    def set_nfolds(self):
+        try:
+            return self.nfolds_test
+        except Exception as e:
+            print(e)
 
-    def get_testfold(self):
-        return self.testfold_test
+    def do_strat_folds(self,datakey=None,nfolds=None,testfold=None,colname=None):
+        self.data[datakey].stratified_folds(nfolds=nfolds,sortby=colname)
+        self.data[datakey+'-Train']=self.data[datakey].rows_match(('meta', 'Folds'), [testfold], invert=True)
+        self.data[datakey+'-Test']=self.data[datakey].rows_match(('meta', 'Folds'), [testfold])
+        print(self.data.keys())
+        print(self.data[datakey+'-Test'].df.index.shape)
+        print(self.data[datakey+'-Train'].df.index.shape)
+        
+    
+    def set_testfold(self):
+        try:
+            return self.testfold_test
+        except Exception as e:
+            print(e)
 
     def set_compranges(self, compranges):
-        self.compranges = compranges
-        print("{}".format(compranges))
-        print("Submodel ranges has been applied")
+        try:
+            self.compranges = compranges
+            print("{}".format(compranges))
+            print("Submodel ranges has been applied")
+        except Exception as e:
+            print(e)
 
     def set_stratified(self):
-        print("Beginning stratification of data")
-        self.data.stratified_folds(nfolds=self.nfolds_test, sortby=('meta', self.el))
-        self.data1_train = self.data.rows_match(('meta', 'Folds'), [self.testfold_test], invert=True)
-        self.data1_test = self.data.rows_match(('meta', 'Folds'), [self.testfold_test])
-        print("Finishing up on stratification")
+        try:
+            print("Beginning stratification of data")
+            self.data.stratified_folds(nfolds=self.nfolds_test, sortby=('meta', self.el))
+            self.data1_train = self.data.rows_match(('meta', 'Folds'), [self.testfold_test], invert=True)
+            self.data1_test = self.data.rows_match(('meta', 'Folds'), [self.testfold_test])
+            print("Finishing up on stratification")
+        except Exception as e:
+            print(e)
 
     def get_number_components(self, ncs):
         # ncs = [7, 7, 5, 9]
-        self.ncs = ncs
-        print("{}".format(ncs))
-        print("Applying components")
+        try:
+            self.ncs = ncs
+            print("{}".format(ncs))
+            print("Applying components")
+        except Exception as e:
+            print(e)
 
-    def get_train_data(self):
-        print("Training has begun on data")
-        self.traindata = [self.data1_train.df, self.data1_train.df, self.data1_train.df, self.data1_train.df]
-        print("Finishing up on training")
-
-    def get_test_data(self):
-        print("Training has begun on test set")
-        self.testdata = [self.data1_test.df, self.data1_test.df, self.data1_test.df, self.data1_test.df]
-        print("Finishing up on training")
 
     def set_sm(self):
         self.sm = pls_sm()
@@ -109,9 +173,10 @@ class pysat_func():
     def get_plots(self):
         print("Now outputting plots to output folder")
         self.sm.final(self.testdata[0]['meta'][self.el],
-        self.blended_test,
-        el=self.el,
-        xcol='Ref Comp Wt. %',
-        ycol='Predicted Comp Wt. %',
-        figpath=self.outpath)
+                  self.blended_test,
+                  el=self.el,
+                  xcol='Ref Comp Wt. %',
+                  ycol='Predicted Comp Wt. %',
+                  figpath=self.outpath)
         print("All finished")
+
