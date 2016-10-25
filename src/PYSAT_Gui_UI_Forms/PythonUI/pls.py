7df15c42814ab7e906ec3f4a0d76482cafdcd4c6
Nicholas Finch <ngf4@nau.edu>
2016-10-25 15:17:49 -0700

    Adding UI forms 	new file:   src/PYSAT_Gui_UI_Forms/01_mainwindow_no_menu.ui 	new file:   src/PYSAT_Gui_UI_Forms/PythonUI/01_mainwindow_no_menu.py 	new file:   src/PYSAT_Gui_UI_Forms/PythonUI/pls.py

---
+++ src/PYSAT_Gui_UI_Forms/PythonUI/pls.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\nfinch\Desktop\GitHub\PySAT\src\PYSAT_Gui_UI_Forms\pls.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(254, 54)
+        self.spinBox_2 = QtGui.QSpinBox(Dialog)
+        self.spinBox_2.setGeometry(QtCore.QRect(120, 10, 81, 22))
+        self.spinBox_2.setObjectName(_fromUtf8("spinBox_2"))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(10, 10, 111, 16))
+        self.label.setObjectName(_fromUtf8("label"))
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.label.setText(_translate("Dialog", "# of components", None))
+
