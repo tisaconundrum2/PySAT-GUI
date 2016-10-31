c87232e7c04a9dcae0591856db9d1e7f6452e010
Nicholas Finch <ngf4@nau.edu>
2016-10-31 15:54:36 -0700

    Changes to be committed: Fixed naming convention 	modified:   src/PYSAT_Gui_UI_Forms/UI Files/01_mainwindow_regression.ui Organized the code, and put prediction back in as it seems to not work if it's not in there 	modified:   src/Python PYSAT_GUI/UnitTests.py

--- src/Python PYSAT_GUI/UnitTests.py
+++ src/Python PYSAT_GUI/UnitTests.py
@@ -10,32 +10,47 @@ pysat = pysat_func()
 fun_list = []
 arg_list = []
 kw_list = []
-# C:\Users\nfinch\Desktop\full_db_mars_corrected_dopedTiO2_pandas_format.csv
-# C:\Users\nfinch\Desktop\lab_data_averages_pandas_format.csv
-# C:\Users\nfinch\Desktop\mask_minors_noise.csv
-arg_list.append([r"C:\Users\nfinch\Desktop\Output"])                                                                    # set_file_outpath
-arg_list.append([r"C:\Users\nfinch\Desktop\full_db_mars_corrected_dopedTiO2_pandas_format.csv", "known data"])          # get_dat1a
-arg_list.append(['known data', r"C:\Users\nfinch\Desktop\mask_minors_noise.csv"])                                       # do_mask
-arg_list.append(['known data', [(0, 1000)]])                                                                            # do_norm
-arg_list.append(['known data', 5, 2, ('comp', 'SiO2')])                                                                 # do_strat_folds
-arg_list.append(['known data', 'wvl', ('comp', 'SiO2'), 'PLS', {'n_components': 7, 'scale': False}, {}])                # do_regression_train
-arg_list.append(['known data', ('comp', 'SiO2'), ('comp', 'PLS_prediction'), 'PLS_SiO2_nc7.png'])                       # do_scatterplot
 
-kw_list.append({})                                                                                                      # set_file_outpath
-kw_list.append({})                                                                                                      # get_dat1a
-kw_list.append({})                                                                                                      # do_mask
-kw_list.append({})                                                                                                      # do_norm
-kw_list.append({})                                                                                                      # do_strat_folds
-kw_list.append({})                                                                                                      # do_regression_train
-kw_list.append({'one_to_one': True})                                                                                    # do_scatterplot
-
+# 0
 fun_list.append(pysat.set_file_outpath)
+arg_list.append([r"C:\Users\nfinch\Desktop\Output"])
+kw_list.append({})
+
+# 1
 fun_list.append(pysat.get_data)
+arg_list.append([r"C:\Users\nfinch\Desktop\full_db_mars_corrected_dopedTiO2_pandas_format.csv", "known data"])
+kw_list.append({})
+
+# 2
 fun_list.append(pysat.do_mask)
+arg_list.append(['known data', r"C:\Users\nfinch\Desktop\mask_minors_noise.csv"])
+kw_list.append({})
+
+# 3
 fun_list.append(pysat.submodel_ranges)
+arg_list.append(['known data', [(0, 1000)]])
+kw_list.append({})
+
+# 4
 fun_list.append(pysat.do_strat_folds)
+arg_list.append(['known data', 5, 2, ('meta', 'SiO2')])
+kw_list.append({})
+
+# 5
 fun_list.append(pysat.do_regression_train)
+arg_list.append(['known data', 'wvl', ('meta', 'SiO2'), 'PLS', {'n_components': 7, 'scale': False}, {}])
+kw_list.append({})
+
+# 6
+fun_list.append(pysat.do_regression_predict)
+arg_list.append(['known data', 'PLS', 'wvl'])
+kw_list.append({})
+
+# 7
 fun_list.append(pysat.do_scatterplot)
+arg_list.append(['known data', ('meta', 'SiO2'), ('meta', 'PLS_prediction'), 'PLS_SiO2_nc7.png'])
+kw_list.append({'one_to_one': True})
+
 
 for i in range(len(fun_list)):
     print(i)
