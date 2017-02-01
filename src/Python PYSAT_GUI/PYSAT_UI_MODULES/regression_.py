7bee764e837697706d6eade0518815bf0d432243
Ryan Anderson <rbanderson@usgs.gov>
2017-02-01 14:25:27 -0700

    submodel UI pieces connected to each other
    Not yet connected to back-end functionality

--- src/Python PYSAT_GUI/PYSAT_UI_MODULES/regression_.py
+++ src/Python PYSAT_GUI/PYSAT_UI_MODULES/regression_.py
@@ -54,7 +54,7 @@ class regression_:
             if method == 'PLS':
                 params = {'n_components': self.reg_widget.pls_nc_spinbox.value(),
                           'scale': False}
-                modelkey =  modelkey+ str(params)
+                modelkey =  modelkey+ '( nc='+str(params['n_components'])+')'
                 kws = {'modelkey': modelkey}
             if method == 'GP':
                 params = {'reduce_dim': self.reg_widget.gp_dim_red_combobox.currentText(),
