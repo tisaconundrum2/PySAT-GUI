496feca3ce2568afc2bec0877be418d137714748
Nicholas Finch <ngf4@nau.edu>
2017-06-01 16:02:20 -0700

    fixing autoGitter

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -2,7 +2,7 @@
 :top
 for /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
 	%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT_Point_Spectra_GUI.git
-	%%I pull origin dev
+ 	%%I pull origin dev
 	%%I add -A
 	%%I commit -m "Auto Generated Update"
 	%%I push -u origin dev
