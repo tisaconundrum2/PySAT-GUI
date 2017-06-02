fbc9daf9b3d9036e88eadf92c976f1f78aaac28c
Nicholas Finch <ngf4@nau.edu>
2017-06-02 12:23:21 -0700

    Automatically push any commits I make

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -2,10 +2,7 @@
 :top
 for /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
 	%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT_Point_Spectra_GUI.git
- 	%%I pull origin dev
-	%%I add -A
-	%%I commit -m "Auto Generated Update"
 	%%I push -u origin dev
-	timeout /t 600
+	timeout /t 60
 )
 goto :top
