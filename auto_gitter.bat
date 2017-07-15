c3e0134a5f39b425bca042ad6afdef4e0205f64d
tisaconundrum2 <ngf4@nau.edu>
2017-07-14 17:20:52 -0700

    adding squash so I can autosquash in the future

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -1,8 +1,14 @@
 @echo off
 :top
-for /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
-	%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT_Point_Spectra_GUI.git
-	%%I push -u origin dev
-	timeout /t 60
+
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+FOR /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
+%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT_Point_Spectra_GUI.git
+%%I  pull origin master
+%%I  add -A
+%%I  commit -m "squash %mydate%_%mytime%"
+timeout /t 60
 )
 goto :top
