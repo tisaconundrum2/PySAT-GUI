84e884e4c69408d935e4dc32a2fbe2b051ac5634
Nicholas Finch <ngf4@nau.edu>
2017-07-28 21:23:00 -0700

    Update 2017-07-28_0923 PM

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -4,11 +4,9 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-FOR /f "delims=" %%I in ('dir %USERPROFILE% /b/o/w/s ^| find /i "cmd\git.exe"') do (
-%%I config remote.origin.url git@github.com:tisaconundrum2/PySAT_Point_Spectra_GUI.git
-%%I  pull origin master
-%%I  add -A
-%%I  commit -m "Update %mydate%_%mytime%"
-timeout /t 60
-)
+git reset head~
+git add -A
+git commit -m "Update %mydate%_%mytime%"
+git push -f
+pause
 goto :top
