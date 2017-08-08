a6d2dd86f2957320cb605820d07a446c13d51848
tisaconundrum2 <ngf4@nau.edu>
2017-08-08 16:34:51 -0700

    dictating what is UI

--- point_spectra_gui/future_/__main__.py
+++ point_spectra_gui/future_/__main__.py
@@ -2,14 +2,14 @@ import sys
 
 from PyQt5 import QtWidgets
 
-from point_spectra_gui.future_.ui import MainWindow, RegressionTrain
+from point_spectra_gui.future_.ui import MainWindow, UI_RegressionTrain
 
 app = QtWidgets.QApplication(sys.argv)
 mw = QtWidgets.QMainWindow()
 mainW = MainWindow.Ui_MainWindow()
 mainW.setupUi(mw)
 
-widget1 = RegressionTrain.Ui_Form()
+widget1 = UI_RegressionTrain.Ui_Form()
 widget1.setupUi(mainW.scrollArea)
 # widget2 = RegressionPredict.Ui_Form()
 # widget2.setupUi(mainW.scrollArea)
