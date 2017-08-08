6000239536a5b9ab894f7882af8afe1b42214add
tisaconundrum2 <ngf4@nau.edu>
2017-08-07 18:20:28 -0700

    Perhaps making a class?

--- point_spectra_gui/future_/__main__.py
+++ point_spectra_gui/future_/__main__.py
@@ -2,17 +2,9 @@ import sys
 from PyQt5 import QtWidgets
 from point_spectra_gui.future_ import *
 
-app = QtWidgets.QApplication(sys.argv)
-mw = QtWidgets.QMainWindow()
-mainW = MainWindow.Ui_MainWindow()
-mainW.setupUi(mw)
+class MyMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MyMainWindow,self).__init__(parent)
+        self.form = MainWindow.Ui_MainWindow()
+        self.setCentralWidget(self.form)
 
-widget1 = RegressionTrain.Ui_Form()
-widget1.setupUi(mainW.scrollArea)
-# widget2 = RegressionPredict.Ui_Form()
-# widget2.setupUi(mainW.scrollArea)
-
-
-
-mw.show()
-sys.exit(app.exec_())
