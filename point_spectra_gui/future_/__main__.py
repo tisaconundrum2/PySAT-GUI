cee7fe6c80e1a5203f7173aa7caaf6a1477979a1
tisaconundrum2 <ngf4@nau.edu>
2017-08-08 16:25:51 -0700

    adding all UI files to dedicated folder

--- point_spectra_gui/future_/__main__.py
+++ point_spectra_gui/future_/__main__.py
@@ -1,10 +1,20 @@
 import sys
+
 from PyQt5 import QtWidgets
-from point_spectra_gui.future_ import *
 
-class MyMainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super(MyMainWindow,self).__init__(parent)
-        self.form = MainWindow.Ui_MainWindow()
-        self.setCentralWidget(self.form)
+from point_spectra_gui.future_.ui import MainWindow, RegressionTrain
 
+app = QtWidgets.QApplication(sys.argv)
+mw = QtWidgets.QMainWindow()
+mainW = MainWindow.Ui_MainWindow()
+mainW.setupUi(mw)
+
+widget1 = RegressionTrain.Ui_Form()
+widget1.setupUi(mainW.scrollArea)
+# widget2 = RegressionPredict.Ui_Form()
+# widget2.setupUi(mainW.scrollArea)
+
+
+
+mw.show()
+sys.exit(app.exec_())
