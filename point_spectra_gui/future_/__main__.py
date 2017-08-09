ed47951fa80895f849bd6343e252b6aabed1b5de
tisaconundrum2 <ngf4@nau.edu>
2017-08-09 12:05:56 -0700

    Success!! Add UI pieces without altering the UI file
    By using a parent/child hierarchy I can override the parent's functionality and slip in my own functions that the parent UI usually doesn't have!
    
    With this I can begin adding in the UI to __main__ file

--- point_spectra_gui/future_/__main__.py
+++ point_spectra_gui/future_/__main__.py
@@ -2,17 +2,16 @@ import sys
 
 from PyQt5 import QtWidgets
 
-from point_spectra_gui.future_.ui import MainWindow, UI_RegressionTrain
+from point_spectra_gui.future_.ui import MainWindow, UI_RegressionTrain, RegressionTrain
 
 app = QtWidgets.QApplication(sys.argv)
 mw = QtWidgets.QMainWindow()
 mainW = MainWindow.Ui_MainWindow()
 mainW.setupUi(mw)
 
-widget1 = UI_RegressionTrain.Ui_Form()
-widget1.setupUi(mainW.scrollArea)
-# widget2 = RegressionPredict.Ui_Form()
-# widget2.setupUi(mainW.scrollArea)
+widget1 = RegressionTrain.RegressionTrain()
+widget1.setupUi(mainW.centralwidget)
+mainW.widgetLayout.addWidget(widget1.get_widget())
 
 
 
