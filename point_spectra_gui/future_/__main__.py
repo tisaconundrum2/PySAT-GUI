bab5c60e6d2374f79f6b97ba7ca5677a3893e72f
tisaconundrum2 <ngf4@nau.edu>
2017-08-09 12:27:52 -0700

    Standardizing classes among UI's
    If we're going to inherit from the parent, we should inherit all the features. This way it's easier to just call one thing instead of having to deal with two.

--- point_spectra_gui/future_/__main__.py
+++ point_spectra_gui/future_/__main__.py
@@ -9,11 +9,11 @@ mw = QtWidgets.QMainWindow()
 mainW = MainWindow.Ui_MainWindow()
 mainW.setupUi(mw)
 
-regression_train = RegressionTrain.RegressionTrain()
+regression_train = RegressionTrain.Ui_Form()
 regression_train.setupUi(mainW.centralwidget)
 mainW.widgetLayout.addWidget(regression_train.get_widget())
 
-dim_red = DimenstionalityReduction.DimenstionalityReduction()
+dim_red = DimenstionalityReduction.Ui_Form()
 dim_red.setupUi(mainW.centralwidget)
 mainW.widgetLayout.addWidget(dim_red.get_widget())
 
--- point_spectra_gui/future_/ui/RegressionTrain.py
+++ point_spectra_gui/future_/ui/RegressionTrain.py
@@ -1,7 +1,7 @@
 from point_spectra_gui.future_.ui.UI_RegressionTrain import Ui_Form
 
 
-class RegressionTrain(Ui_Form):
+class Ui_Form(Ui_Form):
     def setupUi(self, Form):
         super().setupUi(Form)
 
