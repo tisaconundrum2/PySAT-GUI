43ff833cc16da158cdb5dc48f5e369503c0b60ea
tisaconundrum2 <ngf4@nau.edu>
2017-08-15 10:53:48 -0700

    Adding timed unit

--- auto_gitter.bat
+++ auto_gitter.bat
@@ -4,5 +4,5 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 git add -A
 git commit -m "Update %mydate%_%mytime%"
-pause
+sleep 300
 goto :top
