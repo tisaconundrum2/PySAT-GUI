9e86e719a5c1e5654bf69e89eadaceddf2c1855f
tisaconundrum2 <ngf4@nau.edu>
2017-08-21 17:37:08 -0700

    Adding connectWidgets
    This for some reason could not be placed within the child MainWindow class...

--- point_spectra_gui/future_/__main__.py
+++ point_spectra_gui/future_/__main__.py
@@ -10,12 +10,38 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 
 from point_spectra_gui.future_.functions import *
+from point_spectra_gui.future_.util import delete
 from point_spectra_gui.future_.util.excepthook import my_exception_hook
 
 
 def new():
     p = mp.Process(target=main, args=())
     p.start()
+
+
+def connectWidgets(ui):
+    ui.actionRead_ChemCam_Data.triggered.connect(lambda: ui.addWidget(ReadChemCamData.Ui_Form))
+    ui.actionRemove_Baseline.triggered.connect(lambda: ui.addWidget(BaselineRemoval.Ui_Form))
+    ui.actionCross_Validation.triggered.connect(lambda: ui.addWidget(CrossValidation.Ui_Form))
+    ui.actionDimensionality_Reduction.triggered.connect(lambda: ui.addWidget(DimensionalityReduction.Ui_Form))
+    ui.actionInterpolate.triggered.connect(lambda: ui.addWidget(Interpolation.Ui_Form))
+    ui.actionLoad_Data.triggered.connect(lambda: ui.addWidget(LoadData.Ui_Form))
+    ui.actionApply_Mask.triggered.connect(lambda: ui.addWidget(MaskData.Ui_Form))
+    ui.actionMultiply_by_Vector.triggered.connect(lambda: ui.addWidget(MultiplyByVector.Ui_Form))
+    ui.actionNormalization.triggered.connect(lambda: ui.addWidget(Normalization.Ui_Form))
+    ui.actionSet_Output_Path.triggered.connect(lambda: ui.addWidget(OutputFolder.Ui_Form))
+    ui.actionPeak_Areas.triggered.connect(lambda: ui.addWidget(PeakAreas.Ui_Form))
+    ui.actionPlot.triggered.connect(lambda: ui.addWidget(Plot.Ui_Form))
+    ui.actionPlot_ICA_PCA.triggered.connect(lambda: ui.addWidget(Plot_ICA_PCA.Ui_Form))
+    ui.actionPlot_Spectra.triggered.connect(lambda: ui.addWidget(PlotSpectra.Ui_Form))
+    ui.actionTrain.triggered.connect(lambda: ui.addWidget(RegressionTrain.Ui_Form))
+    ui.actionRemove_Rows.triggered.connect(lambda: ui.addWidget(RemoveRows.Ui_Form))
+    ui.actionSplit_Data.triggered.connect(lambda: ui.addWidget(SplitDataset.Ui_Form))
+    ui.actionStratified_Folds.triggered.connect(lambda: ui.addWidget(StratifiedFolds.Ui_Form))
+    ui.actionSubmodel_Predict.triggered.connect(lambda: ui.addWidget(SubmodelPredict.Ui_Form))
+    ui.deleteModulePushButton.clicked.connect(lambda: delete.del_layout(ui.verticalLayout_3))
+    ui.actionCreate_New_Workflow.triggered.connect(lambda: new())
+
 
 
 def get_splash(app):
@@ -39,7 +65,7 @@ def main():
     mainWindow = QtWidgets.QMainWindow()
     ui = MainWindow.Ui_MainWindow()
     ui.setupUi(mainWindow)
-    ui.actionCreate_New_Workflow.triggered.connect(lambda: new())
+    connectWidgets(ui)
     mainWindow.show()
     sys.exit(app.exec_())
 
--- point_spectra_gui/future_/functions/MainWindow.py
+++ point_spectra_gui/future_/functions/MainWindow.py
@@ -1,20 +1,15 @@
-import sys
 from PyQt5 import QtWidgets
 
-from point_spectra_gui.future_.__main__ import new
-from point_spectra_gui.future_.functions import *
 from point_spectra_gui.ui import MainWindow
 
 
 class Ui_MainWindow(MainWindow.Ui_MainWindow):
     def setupUi(self, MainWindow):
-        self.MainWindow = MainWindow
         super().setupUi(MainWindow)  # Run the basic window UI
         self.menu_item_shortcuts()  # set up the shortcuts
-        self.menuItemFunctions()
 
     def addWidget(self, object):
-        widget = object.Ui_Form()
+        widget = object()
         widget.setupUi(self.scrollArea)
         self.widgetLayout = QtWidgets.QVBoxLayout()
         self.widgetLayout.setObjectName("widgetLayout")
@@ -27,26 +22,3 @@ class Ui_MainWindow(MainWindow.Ui_MainWindow):
         self.actionOpen_Workflow.setShortcut("ctrl+O")
         self.actionRestore_Workflow.setShortcut("ctrl+R")
         self.actionSave_Current_Workflow.setShortcut("ctrl+S")
-
-    def menuItemFunctions(self):
-        self.actionRead_ChemCam_Data.triggered.connect(lambda: self.addWidget(ReadChemCamData))
-        self.actionRemove_Baseline.triggered.connect(lambda: self.addWidget(BaselineRemoval))
-        self.actionCross_Validation.triggered.connect(lambda: self.addWidget(CrossValidation))
-        self.actionDimensionality_Reduction.triggered.connect(lambda: self.addWidget(DimensionalityReduction))
-        self.actionInterpolate.triggered.connect(lambda: self.addWidget(Interpolation))
-        self.actionLoad_Data.triggered.connect(lambda: self.addWidget(LoadData))
-        self.actionApply_Mask.triggered.connect(lambda: self.addWidget(MaskData))
-        self.actionMultiply_by_Vector.triggered.connect(lambda: self.addWidget(MultiplyByVector))
-        self.actionNormalization.triggered.connect(lambda: self.addWidget(Normalization))
-        self.actionSet_Output_Path.triggered.connect(lambda: self.addWidget(OutputFolder))
-        self.actionPeak_Areas.triggered.connect(lambda: self.addWidget(PeakAreas))
-        self.actionPlot.triggered.connect(lambda: self.addWidget(Plot))
-        self.actionPlot_ICA_PCA.triggered.connect(lambda: self.addWidget(Plot_ICA_PCA))
-        self.actionPlot_Spectra.triggered.connect(lambda: self.addWidget(PlotSpectra))
-        self.actionTrain.triggered.connect(lambda: self.addWidget(RegressionTrain))
-        self.actionRemove_Rows.triggered.connect(lambda: self.addWidget(RemoveRows))
-        self.actionSplit_Data.triggered.connect(lambda: self.addWidget(SplitDataset))
-        self.actionStratified_Folds.triggered.connect(lambda: self.addWidget(StratifiedFolds))
-        self.actionSubmodel_Predict.triggered.connect(lambda: self.addWidget(SubmodelPredict))
-        # self.deleteModulePushButton.clicked.connect(lambda: del_qwidget(self.w))
-
