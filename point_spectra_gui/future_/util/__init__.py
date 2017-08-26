fd48065a4328162c73d90a071203cdecc111f33f
tisaconundrum2 <ngf4@nau.edu>
2017-08-25 18:23:35 -0700

    name change

--- point_spectra_gui/future_/util/__init__.py
+++ point_spectra_gui/future_/util/__init__.py
@@ -1,3 +1,3 @@
 from point_spectra_gui.future_.util import delete
 from point_spectra_gui.future_.util import excepthook
-from point_spectra_gui.future_.util import moduleLinker
+from point_spectra_gui.future_.util import module_linker
