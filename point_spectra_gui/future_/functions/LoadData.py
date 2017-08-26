259c7fb2dda6f501d75773c13a93e5da5865f2ad
tisaconundrum2 <ngf4@nau.edu>
2017-08-25 18:23:39 -0700

    Figuring out how to output parameters

--- point_spectra_gui/future_/functions/LoadData.py
+++ point_spectra_gui/future_/functions/LoadData.py
@@ -17,7 +17,7 @@ class Ui_Form(Ui_loadData):
 
     def connectWidgets(self):
         self.qt = Qtickle.Qtickle(self)
-        self.qt.isGuiChanged(self.qt.guiSave)
+        self.qt.isGuiChanged(self.getGuiParams)
         print(Ui_Form.uiID)
         self.newFilePushButton.clicked.connect(lambda: self.on_getDataButton_clicked(self.fileNameLineEdit))
 
@@ -26,3 +26,8 @@ class Ui_Form(Ui_loadData):
         lineEdit.setText(filename)
         if lineEdit.text() == "":
             lineEdit.setText("*.csv")
+
+    def getGuiParams(self):
+        # TODO put the parameters inside of a list/dictionary
+        # TODO create a function that loads in the necessary module
+        print(self.qt.guiSave())
