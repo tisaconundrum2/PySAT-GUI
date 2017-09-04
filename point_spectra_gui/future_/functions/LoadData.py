adcee2d287c4bf02a6de5809ba8fd62e233e5451
Nicholas Finch <ngf4@nau.edu>
2017-09-04 11:03:29 -0700

    error_print crashes out the application, deleting

--- point_spectra_gui/future_/functions/LoadData.py
+++ point_spectra_gui/future_/functions/LoadData.py
@@ -4,7 +4,6 @@ from pysat.spectral.spectral_data import spectral_data
 
 from point_spectra_gui.future_.util.BasicFunctionality import Basics
 from point_spectra_gui.ui.LoadData import Ui_loadData
-from point_spectra_gui.ui_modules.Error_ import error_print
 
 
 class Ui_Form(Ui_loadData, Basics):
@@ -35,10 +34,10 @@ class Ui_Form(Ui_loadData, Basics):
         keyname = params['dataSetNameLineEdit']
         try:
             print('Loading data file: ' + str(filename))
-            Basics.data[keyname] = spectral_data(pd.read_csv(filename, header=[0, 1]))
             Basics.datakeys.append(keyname)
+            Basics.data[keyname] = spectral_data(pd.read_csv(filename, header=[0, 1]))
         except Exception as e:
-            error_print('Problem reading data: {}'.format(e))
+            print('Problem reading data: {}'.format(e))
 
     def setDisabled(self, bool):
         self.get_widget().setDisabled(bool)
