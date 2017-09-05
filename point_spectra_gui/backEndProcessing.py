42e803a0c9f56a7ccb738cd0ca619c622d49b5a1
Nicholas Finch <ngf4@nau.edu>
2017-09-04 20:47:17 -0700

    Adding split data

--- point_spectra_gui/backEndProcessing.py
+++ point_spectra_gui/backEndProcessing.py
@@ -34,22 +34,6 @@ class backEndProc(QThread):
         self.greyed_modules = []
         self.outpath = './'
 
-    def do_split_data(self, datakey, colname):
-        try:
-            vars_level0 = self.data[datakey].df.columns.get_level_values(0)
-            vars_level1 = self.data[datakey].df.columns.get_level_values(1)
-            vars_level1 = list(vars_level1[vars_level0 != 'wvl'])
-            vars_level0 = list(vars_level0[vars_level0 != 'wvl'])
-            colname = (vars_level0[vars_level1.index(colname)], colname)
-
-            coldata = np.array([str(i) for i in self.data[datakey].df[colname]])
-            unique_values = np.unique(coldata)
-            for i in unique_values:
-                new_datakey = datakey + ' - ' + str(i)
-                self.datakeys.append(new_datakey)
-                self.data[new_datakey] = spectral_data(self.data[datakey].df.ix[coldata == i])
-        except Exception as e:
-            print(e)
 
     def do_peak_area(self, datakey, peaks_mins_file):
         try:
--- point_spectra_gui/future_/functions/SplitDataset.py
+++ point_spectra_gui/future_/functions/SplitDataset.py
@@ -1,3 +1,6 @@
+import numpy as np
+from spectral.spectral_data import spectral_data
+
 from point_spectra_gui.future_.util.BasicFunctionality import Basics
 from point_spectra_gui.ui.SplitDataset import Ui_Form
 
@@ -15,3 +18,23 @@ class Ui_Form(Ui_Form, Basics):
 
     def setDisabled(self, bool):
         self.get_widget().setDisabled(bool)
+
+    def function(self):
+        params = self.getGuiParams()
+        datakey = params['chooseDataComboBox']
+        colname = params['splitOnUniqueValuesOfComboBox']
+        try:
+            vars_level0 = self.data[datakey].df.columns.get_level_values(0)
+            vars_level1 = self.data[datakey].df.columns.get_level_values(1)
+            vars_level1 = list(vars_level1[vars_level0 != 'wvl'])
+            vars_level0 = list(vars_level0[vars_level0 != 'wvl'])
+            colname = (vars_level0[vars_level1.index(colname)], colname)
+
+            coldata = np.array([str(i) for i in self.data[datakey].df[colname]])
+            unique_values = np.unique(coldata)
+            for i in unique_values:
+                new_datakey = datakey + ' - ' + str(i)
+                self.datakeys.append(new_datakey)
+                self.data[new_datakey] = spectral_data(self.data[datakey].df.ix[coldata == i])
+        except Exception as e:
+            print(e)
