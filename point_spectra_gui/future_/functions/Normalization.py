19f887c049158c276cc628a85e2ae312ebc9f1bf
tisaconundrum2 <ngf4@nau.edu>
2017-09-08 13:37:25 -0700

    Success, can add wavelengths, global min set

--- point_spectra_gui/future_/functions/Normalization.py
+++ point_spectra_gui/future_/functions/Normalization.py
@@ -3,28 +3,47 @@ from point_spectra_gui.ui.Normalization import Ui_Form
 
 
 class normWidgets:
-    def __init__(self, minimumWavelengthLabel, minimumWavelengthSpinBox, maximumWavelengthLabel,
-                 maximumWavelengthSpinBox):
-        self.minimumWavelengthLabel = minimumWavelengthLabel
-        self.minimumWavelengthSpinBox = minimumWavelengthSpinBox
-        self.maximumWavelengthLabel = maximumWavelengthLabel
-        self.maximumWavelengthSpinBox = maximumWavelengthSpinBox
+    def __init__(self, minimumWLabel, minimumWSpinBox, maximumWLabel, maximumWSpinBox):
+        self.minimumWLabel = minimumWLabel
+        self.minimumWSpinBox = minimumWSpinBox
+        self.maximumWLabel = maximumWLabel
+        self.maximumWSpinBox = maximumWSpinBox
 
     def setHidden(self, bool):
-        self.minimumWavelengthLabel.setHidden(bool)
-        self.minimumWavelengthSpinBox.setHidden(bool)
-        self.maximumWavelengthLabel.setHidden(bool)
-        self.maximumWavelengthSpinBox.setHidden(bool)
-
-    def setMinMax(self):
-        pass
+        self.minimumWLabel.setHidden(bool)
+        self.minimumWSpinBox.setHidden(bool)
+        self.maximumWLabel.setHidden(bool)
+        self.maximumWSpinBox.setHidden(bool)
 
     def getValues(self):
-        return [self.minimumWavelengthSpinBox.text(), self.maximumWavelengthSpinBox.text()]
+        return [int(self.minimumWSpinBox.text()), int(self.maximumWSpinBox.text())]
+
+    def clearValues(self):
+        self.minimumWSpinBox.setValue(0)
+        self.maximumWSpinBox.setValue(0)
+
+    def setMaximum(self, int_):
+        self.minimumWSpinBox.setMaximum(int_)
+        self.maximumWSpinBox.setMaximum(int_)
+
+    def spinBox(self, int_):
+        if int_ == 0:
+            return self.minimumWSpinBox
+        elif int_ == 1:
+            return self.maximumWSpinBox
+        else:
+            return "Not a valid number"
 
 
 class Ui_Form(Ui_Form, Basics):
+    def __init__(self):
+        super().__init__()
+        self.normwidgets = []
+        self.all_boxes = []
+        self.index = 1
+
     def setupUi(self, Form):
+        self.__init__()
         super().setupUi(Form)
         self.connectWidgets()
 
@@ -33,8 +52,11 @@ class Ui_Form(Ui_Form, Basics):
 
     def connectWidgets(self):
         self.setupWidgets()
+        self.setMaximum(9999999)
         self.setHidden(self.normwidgets)
-        self.addRangePushButton.clicked.connect()
+        self.qt.isGuiChanged(self.checkForNewMax)
+        self.addRangePushButton.clicked.connect(self.on_addRange_pushed)
+        self.deleteRangePushButton.clicked.connect(self.on_deleteRange_pushed)
 
     def setDisabled(self, bool):
         self.get_widget().setDisabled(bool)
@@ -43,8 +65,29 @@ class Ui_Form(Ui_Form, Basics):
         for i in range(1, len(list)):
             list[i].setHidden(True)
 
+    def setMaximum(self, int_):
+        for items in self.normwidgets:
+            items.setMaximum(int_)
+
+    def getGuiParams(self):
+        print(self.qt.guiSave())
+
+    def on_addRange_pushed(self):
+        if self.index < len(self.normwidgets):
+            self.normwidgets[self.index].setHidden(False)
+            self.index += 1
+        else:
+            print("Cannot add more wavelengths")
+
+    def on_deleteRange_pushed(self):
+        if self.index > 1:
+            self.index -= 1
+            self.normwidgets[self.index].setHidden(True)
+            self.normwidgets[self.index].clearValues()
+        else:
+            print("Cannot delete any more wavelengths")
+
     def setupWidgets(self):
-        self.normwidgets = []
         self.normwidgets.append(normWidgets(self.minimumWavelengthLabel, self.minimumWavelengthSpinBox,
                                             self.maximumWavelengthLabel, self.maximumWavelengthSpinBox))
         self.normwidgets.append(normWidgets(self.minimumWavelengthLabel_2, self.minimumWavelengthSpinBox_2,
@@ -75,3 +118,10 @@ class Ui_Form(Ui_Form, Basics):
                                             self.maximumWavelengthLabel_14, self.maximumWavelengthSpinBox_14))
         self.normwidgets.append(normWidgets(self.minimumWavelengthLabel_15, self.minimumWavelengthSpinBox_15,
                                             self.maximumWavelengthLabel_15, self.maximumWavelengthSpinBox_15))
+        for i in range(len(self.normwidgets) - 1):
+            for j in range(0, 2):
+                self.all_boxes.append(self.normwidgets[i].spinBox(j))
+
+    def checkForNewMax(self):
+        for i in range(len(self.all_boxes) - 1):
+            self.all_boxes[i].valueChanged.connect(self.all_boxes[i + 1].setMinimum)
