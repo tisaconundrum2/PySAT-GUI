b69ae4d21315ffb13803e18df38de5158cf3e140
Nicholas Finch <ngf4@nau.edu>
2017-09-15 09:21:33 -0700

    Trying to fix weird problem with CV
    not being correctly read in

--- point_spectra_gui/functions/regressionMethods/Ridge.py
+++ point_spectra_gui/functions/regressionMethods/Ridge.py
@@ -1,3 +1,5 @@
+import ast
+
 from PyQt5 import QtWidgets
 from sklearn.linear_model.ridge import Ridge
 from sklearn.linear_model.ridge import RidgeCV
@@ -25,7 +27,6 @@ class Ui_Form(Ui_Form, Ridge, RidgeCV, Basics):
         self.fitInterceptCheckBox_cv.setChecked(ridgecv.fit_intercept)
         self.normalizeCheckBox_cv.setChecked(ridgecv.normalize)
         self.defaultComboItem(self.scoringComboBox_cv, ridgecv.scoring)
-        self.cvLineEdit_cv.setText(str(ridgecv.cv))
         self.defaultComboItem(self.gCVModeComboBox_cv, ridgecv.gcv_mode)
         self.storeCVValuesCheckBox_cv.setChecked(ridgecv.store_cv_values)
 
@@ -35,7 +36,7 @@ class Ui_Form(Ui_Form, Ridge, RidgeCV, Basics):
         self.fitInterceptCheckBox.setChecked(ridge.fit_intercept)
         self.normalizeCheckBox.setChecked(ridge.normalize)
         self.copyXCheckBox.setChecked(ridge.copy_X)
-        self.defaultComboItem(self.solverComboBox, (ridge.solver))
+        self.defaultComboItem(self.solverComboBox, ridge.solver)
         self.toleranceDoubleSpinBox.setValue(ridge.tol)
         self.randomStateLineEdit.setText(str(ridge.random_state))
 
@@ -52,13 +53,12 @@ class Ui_Form(Ui_Form, Ridge, RidgeCV, Basics):
             r_state = r_attrib[self.randomStateLineEdit.text()]
 
         if self.crossValidateCheckBox:
-            params = {'alphas': [self.alphasLineEdit_cv],
-                      'fit_intercept': [self.fitInterceptCheckBox_cv],
-                      'normalize': [self.normalizeCheckBox_cv],
-                      'scoring': [self.scoringComboBox_cv],
-                      'cv': [self.cvLineEdit_cv],
-                      'gcv_mode': [self.gCVModeComboBox_cv],
-                      'store_cv_values': [self.storeCVValuesCheckBox_cv]}
+            params = {'alphas': [ast.literal_eval(self.alphasLineEdit_cv.text())],
+                      'fit_intercept': [self.fitInterceptCheckBox_cv.isChecked()],
+                      'normalize': [self.normalizeCheckBox_cv.isChecked()],
+                      'scoring': [self.scoringComboBox_cv.currentText()],
+                      'gcv_mode': [self.gCVModeComboBox_cv.currentText()],
+                      'store_cv_values': [self.storeCVValuesCheckBox_cv.isChecked()]}
         else:
             params = {'alpha': [self.alphaDoubleSpinBox.value()],
                       'copy_X': [self.copyXCheckBox.isChecked()],
@@ -67,16 +67,18 @@ class Ui_Form(Ui_Form, Ridge, RidgeCV, Basics):
                       'normalize': [self.normalizeCheckBox.isChecked()],
                       'solver': [self.solverComboBox.currentText()],
                       'tol': [self.toleranceDoubleSpinBox.value()],
-                      'random_state': [r_state]}
-            modelkey = str(params)
-            return params, modelkey
+                      'random_state': [r_state],
+                      'CV': [self.cro.isChecked()]}
+        modelkey = str(params)
+        return params, modelkey
 
-    if __name__ == "__main__":
-        import sys
 
-        app = QtWidgets.QApplication(sys.argv)
-        Form = QtWidgets.QWidget()
-        ui = Ui_Form()
-        ui.setupUi(Form)
-        Form.show()
-        sys.exit(app.exec_())
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
