470109ee093c7212a7a3b1927737cbb9d272b966
tisaconundrum2 <ngf4@nau.edu>
2017-09-22 14:08:56 -0700

    Success, we can write data to csv

--- point_spectra_gui/functions/WriteToCSV.py
+++ point_spectra_gui/functions/WriteToCSV.py
@@ -10,20 +10,21 @@ class Ui_Form(Ui_Form, Basics):
         self.connectWidgets()
 
     def get_widget(self):
-        return self.groupBox
+        return self.groupbox
 
     def connectWidgets(self):
         self.setComboBox(self.chooseDataSetComboBox, self.datakeys)
+        self.setListWidget(self.variablesToWriteListWidget, self.xvar_choices())
+        self.chooseDataSetComboBox.currentIndexChanged.connect(lambda: self.changeComboListVars(self.variablesToWriteListWidget, self.xvar_choices()))
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
 
     def function(self):
-        params = self.getGuiParams()
-        datakey = params['chooseDataSetComboBox']
-        filename = params['specifyAFilenameLineEdit']
+        datakey = self.chooseDataSetComboBox.currentText()
+        filename = self.specifyAFilenameLineEdit.text()
         selected_cols = self.variablesToWriteListWidget.selectedItems()
         cols = []
         for selection in selected_cols:
             cols.append(selection.text())
-
         try:
             datatemp = self.data[datakey].df[cols]
         except:
@@ -34,18 +35,35 @@ class Ui_Form(Ui_Form, Basics):
         except:
             datatemp.to_csv(filename)
 
+    def on_pushButton_clicked(self):
+        filename, _filter = QtWidgets.QFileDialog.getSaveFileName(None, "Save Data", self.outpath, "(*.csv)")
+        self.specifyAFilenameLineEdit.setText(filename)
+        if self.specifyAFilenameLineEdit.text() == "":
+            self.specifyAFilenameLineEdit.setText("output.csv")
+
     def isEnabled(self):
         return self.get_widget().isEnabled()
 
     def setDisabled(self, bool):
         self.get_widget().setDisabled(bool)
 
+    def xvar_choices(self):
+        try:
+            try:
+                xvarchoices = self.data[self.chooseDataSetComboBox.currentText()].df.columns.levels[0].values
+            except:
+                xvarchoices = self.data[self.chooseDataSetComboBox.currentText()].columns.values
+            xvarchoices = [i for i in xvarchoices if not 'Unnamed' in i]  # remove unnamed columns from choices
+        except:
+            xvarchoices = ['No valid choices!']
+        return xvarchoices
+
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    
+
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
     ui.setupUi(Form)
