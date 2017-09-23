dc6c14475c7c1b4637be07f1797f3aab42bc6ab7
Nicholas Finch <ngf4@nau.edu>
2017-09-23 13:53:15 -0700

    Updating Elastic net to match sklearn

--- point_spectra_gui/functions/regressionMethods/ElasticNet.py
+++ point_spectra_gui/functions/regressionMethods/ElasticNet.py
@@ -1,15 +1,16 @@
 from PyQt5 import QtWidgets
 from sklearn.linear_model import ElasticNet
+from sklearn.linear_model.coordinate_descent import ElasticNetCV
 
 from point_spectra_gui.ui.ElasticNet import Ui_Form
 from point_spectra_gui.util.BasicFunctionality import Basics
 
 
-class Ui_Form(Ui_Form, ElasticNet, Basics):
+class Ui_Form(Ui_Form, ElasticNet, ElasticNetCV, Basics):
     def setupUi(self, Form):
         super().setupUi(Form)
-        self.connectWidgets()
         self.checkMinAndMax()
+        self.connectWidgets()
 
     def get_widget(self):
         return self.groupBox
@@ -18,14 +19,40 @@ class Ui_Form(Ui_Form, ElasticNet, Basics):
         self.get_widget().setHidden(bool)
 
     def connectWidgets(self):
-        self.alphaDoubleSpinBox.setValue(self.alpha),
-        self.l1RatioDoubleSpinBox.setValue(self.l1_ratio),
-        self.fitInterceptCheckBox.setChecked(self.fit_intercept),
-        self.normalizeCheckBox.setChecked(self.normalize),
-        self.maxNumOfIterationsSpinBox.setValue(self.max_iter),
-        self.copyXCheckBox.setChecked(self.copy_X),
-        self.toleranceDoubleSpinBox.setValue(self.tol),
-        self.positiveCheckBox.setChecked(self.positive),
+        self.elasticNetCVGroupBox.setHidden(True)
+        en = ElasticNet()
+        encv = ElasticNetCV()
+
+        self.enalphaSpinBox.setValue(en.alpha)
+        self.enl1_ratioDoubleSpinBox.setValue(en.l1_ratio)
+        self.enfit_interceptCheckBox.setChecked(en.fit_intercept)
+        self.ennormalizeCheckBox.setChecked(en.normalize)
+        self.enprecomputeCheckBox.setChecked(en.precompute)
+        self.enmax_iterSpinBox.setValue(en.max_iter)
+        self.encopy_XCheckBox.setChecked(en.copy_X)
+        self.entolDoubleSpinBox.setValue(en.tol)
+        self.enwarm_startCheckBox.setChecked(en.warm_start)
+        self.enpositiveCheckBox.setChecked(en.positive)
+        self.setComboBox(self.enselectionComboBox, ['cyclic', 'random'])
+        self.defaultComboItem(self.enselectionComboBox, en.selection)
+
+        self.l1_ratioDoubleSpinBox.setValue(encv.l1_ratio)
+        self.epsDoubleSpinBox.setValue(encv.eps)
+        self.n_alphasSpinBox.setValue(encv.n_alphas)
+        self.alphasLineEdit.setText(encv.alphas)
+        self.fit_interceptCheckBox.setChecked(encv.fit_intercept)
+        self.normalizeCheckBox.setChecked(encv.normalize)
+        self.setComboBox(self.precomputeComboBox, ['True', 'False', 'auto', 'array-like'])
+        self.defaultComboItem(self.precomputeComboBox, encv.precompute)
+        self.max_iterSpinBox.setValue(encv.max_iter)
+        self.tolDoubleSpinBox.setValue(encv.tol)
+        # (encv.cv)
+        self.copy_XCheckBox.setChecked(encv.copy_X)
+        self.verboseCheckBox.setChecked(encv.verbose)
+        self.n_jobsSpinBox.setValue(encv.n_jobs)
+        self.positiveCheckBox.setChecked(encv.positive)
+        self.setComboBox(self.selectionComboBox, ['cyclic', 'random'])
+        self.defaultComboItem(self.selectionComboBox, encv.selection)
 
     def function(self):
         p_attrib = {'False': False, 'True': True, 'Array-like': 'array-like'}
@@ -38,19 +65,39 @@ class Ui_Form(Ui_Form, ElasticNet, Basics):
         index = self.precomputeComboBox.currentIndex()
         precomputeComboBox = self.precomputeComboBox.itemText(index)
 
-        params = {
-            'alpha': self.alphaDoubleSpinBox.value(),
-            'l1_ratio': self.l1RatioDoubleSpinBox.value(),
-            'fit_intercept': self.fitInterceptCheckBox.isChecked(),
-            'normalize': self.normalizeCheckBox.isChecked(),
-            'precompute': p_attrib[precomputeComboBox],
-            'max_iter': int(self.maxNumOfIterationsSpinBox.value()),
-            'copy_X': self.copyXCheckBox.isChecked(),
-            'tol': self.toleranceDoubleSpinBox.value(),
-            'positive': self.positiveCheckBox.isChecked(),
-            'selection': self.selectionLineEdit.text(),
-            'random_state': r_state,
-            'CV': self.crossValidateCheckBox.isChecked()}
+        if self.CVCheckBox.isChecked():
+            params = {
+                'alpha': [],
+                'l1_ratio': [],
+                'fit_intercept': [],
+                'normalize': [],
+                'precompute': [],
+                'max_iter': [],
+                'copy_X': [],
+                'tol': [],
+                'warm_start': [],
+                'positive': [],
+                'selection': [],
+                'CV': self.CVCheckBox.isChecked()}
+        else:
+            params = {
+                'l1_ratio': [],
+                'eps': [],
+                'n_alphas': [],
+                'alphas': [],
+                'fit_intercept': [],
+                'normalize': [],
+                'precompute': [],
+                'max_iter': [],
+                'tol': [],
+                'cv': [],
+                'copy_X': [],
+                'verbose': [],
+                'n_jobs': [],
+                'positive': [],
+                'selection': [],
+                'CV': self.CVCheckBox.isChecked()}
+
         modelkey = str(params)
         return params, modelkey
 
