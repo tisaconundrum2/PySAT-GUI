0c75e6558e7e9a0b2ea7765ece05cfc4f8c003b4
Nicholas Finch <ngf4@nau.edu>
2017-09-23 13:58:07 -0700

    single to double spinBox

--- point_spectra_gui/functions/regressionMethods/ElasticNet.py
+++ point_spectra_gui/functions/regressionMethods/ElasticNet.py
@@ -23,7 +23,7 @@ class Ui_Form(Ui_Form, ElasticNet, ElasticNetCV, Basics):
         en = ElasticNet()
         encv = ElasticNetCV()
 
-        self.enalphaSpinBox.setValue(en.alpha)
+        self.enalphaDoubleSpinBox.setValue(en.alpha)
         self.enl1_ratioDoubleSpinBox.setValue(en.l1_ratio)
         self.enfit_interceptCheckBox.setChecked(en.fit_intercept)
         self.ennormalizeCheckBox.setChecked(en.normalize)
