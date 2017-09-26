d25b405cef02ef66a96c9240e596031d173e221c
tisaconundrum <ngf4@nau.edu>
2017-09-26 14:41:51 -0700

    Fixing ignored files

--- auto_gitter.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:top
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-git add -A
-git commit -m "Update %mydate%_%mytime%"
-sleep 300
-goto :top
