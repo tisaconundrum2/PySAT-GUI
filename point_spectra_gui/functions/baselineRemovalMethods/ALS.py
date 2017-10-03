83583eea5718cfe1aedb90237e18d0702904660c
tisaconundrum2 <ngf4@nau.edu>
2017-10-03 16:17:16 -0700

    Fixing ALS

--- point_spectra_gui/functions/baselineRemovalMethods/ALS.py
+++ point_spectra_gui/functions/baselineRemovalMethods/ALS.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-
+from spectral.baseline_code.als import ALS
 from point_spectra_gui.ui.ALS import Ui_Form
 from point_spectra_gui.util.BasicFunctionality import Basics
 
@@ -7,12 +7,21 @@ from point_spectra_gui.util.BasicFunctionality import Basics
 class Ui_Form(Ui_Form, Basics):
     def setupUi(self, Form):
         super().setupUi(Form)
+        self.checkMinAndMax()
+        Basics.setupUi(self, Form)
 
     def get_widget(self):
         return self.groupbox
 
     def setHidden(self, bool):
         self.get_widget().setHidden(bool)
+
+    def connectWidgets(self):
+        als = ALS()
+        self.asymmetryDoubleSpinBox.setValue(als.asymmetry_)
+        self.smoothnessDoubleSpinBox.setValue(als.smoothness_)
+        self.maxNumOfIterationsSpinBox.setValue(als.max_iters_)
+        self.convergenceThresholdDoubleSpinBox.setValue(als.conv_thresh_)
 
     def function(self):
         methodParameters = {'asymmetry_param': float(self.asymmetryDoubleSpinBox.value()),
