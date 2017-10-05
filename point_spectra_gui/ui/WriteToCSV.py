b0eba18ec2c636c308ba6a3529e2397b27b38eff
Nicholas Finch <ngf4@nau.edu>
2017-10-04 21:18:29 -0700

    bad geometries

--- point_spectra_gui/ui/WriteToCSV.py
+++ point_spectra_gui/ui/WriteToCSV.py
@@ -8,7 +8,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(294, 355)
         self.verticalLayout = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout.setObjectName("verticalLayout")
         self.groupBox = QtWidgets.QGroupBox(Form)
